ca4ef924f4c9dbcbd2122fd3ea5df940e9a10014
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-01-08 22:46:39 -0500

    add item images to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,22 @@
       <h2>{{ item.item }}</h2>
       <p>Quantity: {{ item.quantity }}</p>
       <p>Condition: {{ item.condition }}</p>
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <img class="img-cover" v-bind:src="item.image" v-bind:alt="item.item" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.img-cover {
+  height: 250px;
+  width: 40%;
+  object-fit: cover;
+}
+</style>
 
 <script>
 import axios from "axios";

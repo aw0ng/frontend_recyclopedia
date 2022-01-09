15cba9eedf9e31ee242807ca572f7d21d20d4965
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-01-08 22:48:42 -0500

    show donors index on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Donors</h1>
+    <div v-for="donor in donors" v-bind:key="donor.id">
+      <h2>{{ donor.first_name }} {{ donor.last_name }}</h2>
+      <h2>{{ donor.email }}</h2>
+    </div>
   </div>
 </template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      donors: [],
+    };
+  },
+  created: function () {
+    this.indexDonors();
+  },
+  methods: {
+    indexDonors: function () {
+      axios.get("/api/donors").then((response) => {
+        this.donors = response.data;
+        console.log("donors index", response);
+      });
+    },
+  },
+};
+</script>

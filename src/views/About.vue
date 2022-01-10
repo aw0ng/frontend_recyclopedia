d10a6f86f91ed963214245416f95abe6ea338469
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-01-09 22:23:17 -0500

    add images to donors index

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,11 +4,22 @@
     <div v-for="donor in donors" v-bind:key="donor.id">
       <h2>{{ donor.first_name }} {{ donor.last_name }}</h2>
       <h2>{{ donor.email }}</h2>
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <img class="img-cover" v-bind:src="donor.image" v-bind:alt="donor.first_name" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.img-cover {
+  height: 250px;
+  width: 40%;
+  object-fit: cover;
+}
+</style>
 
 <script>
 import axios from "axios";

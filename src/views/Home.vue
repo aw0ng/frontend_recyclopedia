804e6d662a8bf3b94742970bf74631a971d71a6c
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-01-15 22:02:18 -0500

    add categories to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>ALL items</h1>
+    <h1>ALL ITEMS</h1>
     <div v-for="item in items" v-bind:key="item.id">
       <h2>{{ item.item }}</h2>
       <p>Quantity: {{ item.quantity }}</p>
@@ -10,6 +10,10 @@
           <img class="img-cover" v-bind:src="item.image" v-bind:alt="item.item" />
         </div>
       </div>
+    </div>
+    <h1>CATEGORIES</h1>
+    <div v-for="category in categories" v-bind:key="category.id">
+      <h2>{{ category.category_type }}</h2>
     </div>
   </div>
 </template>
@@ -29,16 +33,24 @@ export default {
   data: function () {
     return {
       items: [],
+      categories: [],
     };
   },
   created: function () {
     this.indexItems();
+    this.indexCategories();
   },
   methods: {
     indexItems: function () {
       axios.get("/api/items").then((response) => {
         this.items = response.data;
         console.log("items index", response);
+      });
+    },
+    indexCategories: function () {
+      axios.get("/api/categories").then((response) => {
+        this.categories = response.data;
+        console.log("categories index", response);
       });
     },
   },

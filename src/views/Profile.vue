166eca3438cb8cf133f04ea31e09e68dbdaf6c29
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-01-19 21:44:04 -0500

    look for current donor info - 404 error

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="profile">
     <h1>Profile</h1>
-    <div v-for="donor in donors" v-bind:key="donor.id">
-      <h2>{{ donor.first_name }} {{ donor.last_name }}</h2>
-      <h2>{{ donor.email }}</h2>
-      <div class="row justify-content-center">
-        <div class="col-lg-8">
-          <img class="img-cover" v-bind:src="donor.image" v-bind:alt="donor.first_name" />
-        </div>
+    <!-- <div v-for="donor in donors" v-bind:key="donor.id"> -->
+    <h2>{{ donor.first_name }} {{ donor.last_name }}</h2>
+    <h2>{{ donor.email }}</h2>
+    <div class="row justify-content-center">
+      <div class="col-lg-8">
+        <img class="img-cover" v-bind:src="donor.image" v-bind:alt="donor.first_name" />
       </div>
+      <!-- </div> -->
     </div>
     <body data-first_name="current_donor.first_name" />
   </div>
@@ -28,35 +28,24 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      donors: [],
-      // donor: [],
-      current_donor: [],
+      // donors: [],
+      donor: {},
     };
   },
   created: function () {
-    this.indexDonors();
-    // this.currentDonor();
-    this.data();
+    // this.indexDonors();
+    axios.get("/api/donors" + this.$route.params.id).then((response) => {
+      console.log("donor show", response);
+      this.donor = response.data;
+    });
   },
   methods: {
-    indexDonors: function () {
-      axios.get("/api/donors").then((response) => {
-        this.donors = response.data;
-        console.log("donors index", response);
-      });
-    },
-    // currentDonor: function () {
-    //   axios.get("/api/donors").then((response) => {
-    //     this.donor = response.data.find_by();
-    //     console.log("current user", response);
-    //   });
-    // },
-    data: function () {
-      axios.get("/api/donors").then((response) => {
-        current_donor = data.first_name;
-        console.log("current_donor", response);
-      });
-    },
+    //   indexDonors: function () {
+    //     axios.get("/api/donors").then((response) => {
+    //       this.donors = response.data;
+    //       console.log("donors index", response);
+    //     });
+    //   },
   },
 };
 </script>

eb69909d1d8b156460438015d8c4cc81a540612b
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-01-25 20:13:12 -0500

    fix login response to call donor id and load donor profile

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -54,12 +54,12 @@ export default {
       axios
         .post("/api/sessions", params)
         .then((response) => {
-          axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+          axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.jwt}`;
           localStorage.setItem("jwt", response.data.jwt);
-          this.$router.push("/profile/");
+          this.$router.push(`/profile/${response.data.id}`);
         })
         .catch((error) => {
-          console.log(error.response);
+          console.log(error);
           this.errors = ["Invalid email or password."];
           this.email = "";
           this.password = "";
